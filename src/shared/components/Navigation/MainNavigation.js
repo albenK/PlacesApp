eece894d17dd5d3cd190b905cdf62ac7a7f48a0b
Alben Kalambukadu <albenkalambukadu@Albens-MBP.fios-router.home>
2019-12-26 16:48:29 -0500

    Added app sidedrawer. Added navigation links. Added app header.

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
+import NavLinks from './NavLinks';
+import './MainNavigation.css';
+
+const MainNavigation = (props) => {
+    return (
+        <React.Fragment>
+            <SideDrawer>
+                <nav class="main-navigation__drawer-nav">
+                    <NavLinks/>
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Your places</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks/>
+                </nav>
+            </MainHeader>
+        </React.Fragment>
+    );
+};
+
+export default MainNavigation;
