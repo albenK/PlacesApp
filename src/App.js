648f9fadd54607efb9502cd654fc08198a90f804
Alben Kalambukadu <albenk234@gmail.com>
2021-06-06 14:48:06 -0400

    Added auto logout feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users/Users';
 import NewPlace from './places/pages/NewPlace/NewPlace';
@@ -9,25 +9,62 @@ import Authenticate from './user/pages/Authenticate/Authenticate';
 
 import { AuthContext } from './shared/context/AuthContext';
 
+const USER_DATA_KEY = 'userData';
+let logoutTimer;
 
 const App = () => {
-  const [token, setToken] = useState(null);
-  const [userId, setUserId] = useState(null);
+  const [ token, setToken] = useState(null);
+  const [ tokenExpirationDate, setTokenExpirationDate] = useState(null);
+  const [ userId, setUserId] = useState(null);
 
-  const login = useCallback((id, token) => {
+  const login = useCallback((id, token, expiresIn) => {
     setToken(token);
     setUserId(id);
-    localStorage.setItem('userData', JSON.stringify({ userId: id, token: token }));
+    const oneHourFromNow = new Date().getTime() + (1000 * 60 * 60);
+    const expirationDate = expiresIn || new Date(oneHourFromNow); // token expiration date
+    setTokenExpirationDate(expirationDate);
+    localStorage.setItem(USER_DATA_KEY,
+      JSON.stringify({
+        userId: id,
+        token: token,
+        expiration: expirationDate.toISOString()
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setUserId(null);
     setToken(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem(USER_DATA_KEY);
   }, []);
+
+  // Register an effect to run on mount. Check if we have userData stored.
+  useEffect(() => {
+    const stringifiedData = localStorage.getItem(USER_DATA_KEY);
+    const parsedData = JSON.parse(stringifiedData);
+    const timeNow = new Date().getTime();
+    let isStoredTokenValid = !!parsedData && !!parsedData.token && !!parsedData.expiration ? 
+    timeNow < new Date(parsedData.expiration).getTime() : false;
+    // login if stored token is valid
+    if (isStoredTokenValid) {
+      login(parsedData.userId, parsedData.token, new Date(parsedData.expiration));
+    }
+  }, [login]);
+
+  // Register an effect to auto logout user when token expires.
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, tokenExpirationDate, logout]);
 
   /* Have different routes available based on auth state.*/
   let routes;
-  if (token) {
+  if (token) { // if token exists, user is logged in
     routes = (
       <Switch>
         <Route path="/" exact>
